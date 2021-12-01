6f45349e8ec109b498e167fdcdb9843c35cb1063
Danny Kirchmeier <danny@kirchmeier.us>
2021-11-30 22:43:58 -0800

    Day 01

--- solutions/01.test.ts
+++ solutions/01.test.ts
@@ -1,20 +1,48 @@
 import { TestBuilder } from "../util/test-builder.ts";
 
 const example = `\
-`;
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263`;
 
 new TestBuilder(import.meta)
   .section("a")
-  .example(-1, () => solveA(example))
+  .example(7, () => solveA(example))
   .actual(solveA)
   .section("b")
-  .example(-1, () => solveB(example))
+  .example(5, () => solveB(example))
   .actual(solveB);
 
 function solveA(input: string) {
-  return input.length;
+  const vals = input.split("\n").map(Number);
+  let prev = Number.POSITIVE_INFINITY;
+  let count = 0;
+  for (const v of vals) {
+    if (v > prev) {
+      count++;
+    }
+    prev = v;
+  }
+  return count;
 }
 
 function solveB(input: string) {
-  return input.length;
+  const vals = input.split("\n").map(Number);
+  let prev = vals[0] + vals[1] + vals[2];
+  let count = 0;
+  for (let i = 3; i < vals.length; i++) {
+    const next = prev + vals[i] - vals[i - 3];
+    if (next > prev) {
+      count++;
+    }
+    prev = next;
+  }
+  return count + 1;
 }
