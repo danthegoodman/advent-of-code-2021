687ded2fb65e3e5b20f6ef221914ba0fb972464b
Danny Kirchmeier <danny@kirchmeier.us>
2021-12-01 21:07:13 -0800

    Day 02

--- solutions/02.test.ts
+++ solutions/02.test.ts
@@ -1,20 +1,58 @@
 import { TestBuilder } from "../util/test-builder.ts";
 
 const example = `\
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
 `;
 
 new TestBuilder(import.meta)
   .section("a")
-  .example(-1, () => solveA(example))
+  .example(150, () => solveA(example))
   .actual(solveA)
   .section("b")
-  .example(-1, () => solveB(example))
+  .example(900, () => solveB(example))
   .actual(solveB);
 
 function solveA(input: string) {
-  return input.length;
+  let d = 0;
+  let x = 0;
+  const s = input.split("\n").map((it) => it.split(" "));
+  for (const [name, countS] of s) {
+    const n = Number.parseInt(countS);
+    if (name === "forward") {
+      x += n;
+    }
+    if (name === "down") {
+      d += n;
+    }
+    if (name === "up") {
+      d -= n;
+    }
+  }
+  return x * d;
 }
 
 function solveB(input: string) {
-  return input.length;
+  let d = 0;
+  let x = 0;
+  let a = 0;
+  const s = input.split("\n").map((it) => it.split(" "));
+  for (const [name, countS] of s) {
+    const n = Number.parseInt(countS);
+    if (name === "forward") {
+      x += n;
+      d += n * a;
+    }
+    if (name === "down") {
+      a += n;
+    }
+    if (name === "up") {
+      a -= n;
+    }
+  }
+  return x * d;
 }
